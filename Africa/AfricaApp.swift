a1c11d0e67ba1807517542d0e3a23a9c3c571c4a
Jessica Parsons <jess@junipercreative.co>
2025-01-16 16:28:08 -0800

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Jessica Parsons on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
